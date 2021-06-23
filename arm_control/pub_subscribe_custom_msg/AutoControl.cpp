d0a289f2482dbd45c89e63e41cd7763c05440ccc
TuXun1999 <35623649+TuXun1999@users.noreply.github.com>
2021-06-23 15:37:49 +0800

    Upload my Arduino files in testing the excavator
    Source motivation part included

---
+++ arm_control/pub_subscribe_custom_msg/AutoControl.cpp
@@ -0,0 +1,67 @@
+#include <HardwareSerial.h>
+#include "AutoControl.h"
+
+#include "math.h"
+
+//Constraint the PWM value
+float PWM_Limit(float value, float val_max, float val_min)
+{
+  if(value > val_max) value = val_max;
+  if(value < val_min) value = val_min;
+  return value;
+}
+
+// Calculate the position using the angles 
+void Get_Pos(float boom_angle, float arm_angle)
+{
+  float x = Big_Len * cos((boom_angle + 16)/57.2978f) + Small_Len * cos(arm_angle/57.2978f);
+  float y = Big_Len * sin((boom_angle + 16)/57.2978f) + Small_Len * sin(arm_angle/57.2978f);
+  // Interesting ... what and why is 57.2978 ?
+
+  Serial.print("boom_angle = "); Serial.println((boom_angle-16));
+  Serial.print("small_angle = "); Serial.println(arm_angle);
+  
+  Serial.print("x = "); Serial.println(x);
+  Serial.print("y = "); Serial.println(y);
+}
+
+
+
+//Calculating the arm angles reversely using the coordinates 坐标点逆解算臂角度
+int err=0;
+void Pos_Calcu(float aim_x, float aim_y)
+{
+	float big_angle_rad, small_angle_rad, len, tempA, tempB, tempC, errX, errY;
+  
+	if(aim_y==0) aim_y = 0.001f;
+	len = aim_x * aim_x + aim_y * aim_y;
+
+	if((len>Max_Len||len<Min_Len) || (aim_x>Max_x||aim_x<Min_x) || (aim_y>Max_y||aim_y<Min_y)) err = 1;
+
+	tempA = len - Big_Len * Big_Len + Small_Len * Small_Len;
+	tempB = (-aim_x * sqrt((len-(Big_Len-Small_Len)*(Big_Len-Small_Len)) * ((Big_Len+Small_Len)*(Big_Len+Small_Len)-len)) + tempA * aim_y)/len;
+	tempC = (tempA-aim_y*tempB)/(2*Small_Len*aim_x);
+
+	big_angle_rad   = asin((aim_y - 0.5f*tempB)/Big_Len);
+	if(tempC>0) small_angle_rad = asin(0.5f*tempB/Small_Len);
+	else        small_angle_rad = - asin(0.5f*tempB/Small_Len) - 3.1415926f;
+
+	boom = big_angle_rad   * 57.29578f - 16;
+	arm = small_angle_rad * 57.29578f; //57.29578=180/pi;
+
+  //Constrain the pwm out
+  boom = PWM_Limit(boom, Big_Pwm_Max, Big_Pwm_Min);
+  arm =  PWM_Limit(arm, Small_Pwm_Max, Small_Pwm_Min); 
+
+  
+	errX = Big_Len * cos(big_angle_rad) + Small_Len * cos(small_angle_rad) - aim_x;
+	errY = Big_Len * sin(big_angle_rad) + Small_Len * sin(small_angle_rad) - aim_y;
+	if(fabs(errX) > 1 || fabs(errY) > 1) err = 2;
+  if(err>0) {Serial.print("ERROR:"); Serial.println(err);}
+}
+
+
+
+
+
+
---
+++ arm_control/pub_subscribe_custom_msg/TEST.ino
@@ -0,0 +1,139 @@
+#include <ros.h>
+#include <Servo.h> 
+#include <std_msgs/UInt32.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+
+#include "AutoControl.h"
+
+
+#define Bucket_ServoPin 4
+#define Arm_ServoPin 5
+#define Boom_ServoPin 3
+
+#define Swing_ESCPin 10
+
+
+Servo BUCKET;
+Servo ARM;
+Servo BOOM;
+Servo LEFT_ESC;
+Servo RIGHT_ESC;
+Servo SWING_ESC;
+
+Servo SOURCE_ESC;
+
+#define Source_ESCPin 11
+
+
+
+/******** 初始化 ********/
+void setup()
+{
+  /* 上位机串口初始化 */
+  Serial.begin(9600);
+
+  
+  /* 舵机初始化 */
+  BUCKET.attach(Bucket_ServoPin);
+  ARM.attach(Arm_ServoPin);
+  BOOM.attach(Boom_ServoPin);
+  SWING_ESC.attach(Swing_ESCPin);
+  SOURCE_ESC.attach(Source_ESCPin);
+  
+  BUCKET.writeMicroseconds(200*Bucket_Pwm_Init);
+  ARM.writeMicroseconds(200*Small_Pwm_Init);
+  BOOM.writeMicroseconds(200*Big_Pwm_Init);
+  SWING_ESC.writeMicroseconds(200*Turn_Pwm_Init);
+  SOURCE_ESC.writeMicroseconds(200*Source_Pwm_Init);
+}
+
+
+/******** 主循环 ********/
+void loop()
+{  
+
+  /* 用串口调试助手控制舵机 */
+  static long lastMillis = millis();
+  static float Big_Pwm_Out = Big_Pwm_Init;
+  static float Small_Pwm_Out = Small_Pwm_Init;
+  static float Bucket_Pwm_Out = Bucket_Pwm_Init;
+
+  static float Turn_Pwm_Out = Turn_Pwm_Init;
+
+  static float Source_Pwm_Out = Source_Pwm_Init;
+  bool flag = 0; 
+
+  static float x=60,y=10, angle=10;
+  
+  static uint8_t ch = 0;
+  static uint8_t ch_h = 0;
+  static uint8_t ch_l = 0;
+  /*
+   * 我注释掉了之后可能用来控制机械臂的部分
+   * 现在它就是能转到最大值
+   * 转到用户指定位置的部分之后再说吧。。
+   */
+  /*
+  //Raw input of the position 
+  static uint8_t read_x;
+  static unint8_t read_y;
+  static unint8_t read_angle; 
+  */
+  if(Serial.available()) 
+  {
+    ch = Serial.read();
+    ch_h = ch >> 4;
+    ch_l = ch & 0x0F;
+
+    switch(ch)
+    {
+      case 0x31: Big_Pwm_Out = Big_Pwm_Max; Serial.println("Big_Arm:Down");          break;
+      case 0x32: Big_Pwm_Out = Big_Pwm_Min; Serial.println("Big_Arm:Up");            break;
+      case 0x33: Small_Pwm_Out = Small_Pwm_Min; Serial.println("Small_Arm:Down");        break;
+      case 0x34: Small_Pwm_Out = Small_Pwm_Max; Serial.println("Small_Arm:Up");          break;
+      case 0x35: Bucket_Pwm_Out = Bucket_Pwm_Min; Serial.println("Bucket:Down");           break;
+      case 0x36: Bucket_Pwm_Out = Bucket_Pwm_Max; Serial.println("Bucket:Up");             break;
+      case 0x37: Turn_Pwm_Out = 6.5f;             Serial.println("Turn_Right");            break;
+      case 0x38: Turn_Pwm_Out = 8.5f;             Serial.println("Turn_Left");             break;
+      case 'l': Source_Pwm_Out = 10.00f; Serial.println("Source Gear Max"); break;
+      case 's':  Source_Pwm_Out= 5.00f; Serial.println("Source Gearm Min"); break;
+      case 'm': Source_Pwm_Out = 7.00f; Serial.println("Source Gear Medium"); break;
+      case 'd': Serial.println("Show Source_Pwm_Out:"); Serial.println(Source_Pwm_Out); break;
+      /*
+      case 0x08: Get_Pos(boom, arm);                                         break;
+      case 0x11: read_x = Serial.read(); read_y = Serial.read(); x=(float)read_x; y=(float)read_y; Pos_Calcu(x, y); read_angle = Serial.read(); angle=(float)read_angle; break;
+      case 0x21: Serial.print("Big_Arm_Angle:");       Serial.println(boom);           break;
+      case 0x22: Serial.print("Small_Arm_Angle:");     Serial.println(arm);           break;
+      case 0x23: Serial.print("Bucket_Angle:");        Serial.println(bucket);           break;*/
+      default: /*Source_Pwm_Out = Source_Pwm_Init + 0.8f + (float)ch_l/4; Serial.print("Motor gears:");*/ Serial.println(ch_l); break;
+    }
+
+  }
+
+/*
+  * If the user specified a new coordinate, 
+  * determine the PWM output 
+  * based on the desired angle
+   
+   if(flag){
+    Big_Pwm_Out = boom;
+    Small_Pwm_Out = arm;
+    Turn_Pwm_Out = turn_table; 
+   }*/
+   
+  
+  
+  /* 写舵机占空比 */
+  BOOM.writeMicroseconds(200*Big_Pwm_Out);
+  ARM.writeMicroseconds(200*Small_Pwm_Out);
+  BUCKET.writeMicroseconds(200*Bucket_Pwm_Out);
+  SWING_ESC.writeMicroseconds(200*Turn_Pwm_Out);
+
+  SOURCE_ESC.writeMicroseconds(200*Source_Pwm_Out);
+}
+
+
+
+/******************** finished ********************/
