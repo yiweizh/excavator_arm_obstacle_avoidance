443614ce79adc64bf3656a261f2663321130f6f4
TuXun1999 <35623649+TuXun1999@users.noreply.github.com>
2021-07-05 16:04:02 +0800

    Read three angles (solved multi-value problem)

--- arm_control/pub_subscribe_custom_msg/sensor_arm.ino
+++ arm_control/pub_subscribe_custom_msg/sensor_arm.ino
@@ -22,34 +22,51 @@ TX <---> 0(Rx)
  */
 
  
+ static float raw_stick_angle;
  static float stick_angle;
  static float boom_angle;
+
+ static float raw_base_angle;
  static float base_angle; 
- float read_stick();
- float read_boom();
+ static float base_init;
+ static float boom_stick_angle; 
  
+ static bool location; // 0: RHP; 1: LHP
+ void read_stick();
+ void read_boom_base();
+ float real_base_angle(float raw_base_angle);
 //Set up all the serials
 void setup() 
 {
   Serial.begin(9600);
   serial0.begin(9600);
   JY901_0.StartIIC();
-
+  
+  JY901_0.GetAngle();
+  base_init = (float)JY901_0.stcAngle.Angle[2]/32768*180;
 }
 
 void loop() 
 {
 
     //Read the new data in and process the corresponding data
-    stick_angle = read_stick();
-    base_angle = read_base();
-    boom_angle = read_boom();
+    read_boom_base();
+    read_stick();
+
+    stick_angle = (location == 0)? raw_stick_angle : (-180-raw_stick_angle);
+    base_angle = real_base_angle(raw_base_angle);
+    
       
     //print received data. Data was received in serialEvent;
     Serial.print(stick_angle);
     Serial.print(',');
-      
+    Serial.print(',');
     Serial.print(boom_angle);
+    Serial.print(',');
+
+    //Calculate the angle between the boom and the stick
+    boom_stick_angle = 180-(boom_angle-stick_angle);
+    Serial.print(boom_stick_angle);
     Serial.print(',');
 
     Serial.print(base_angle);
@@ -60,24 +77,32 @@ void loop()
     delay(10);
 }
 
-float read_stick(){
+void read_stick(){
 
    while (serial0.available()) 
   {
     JY901_1.CopeSerialData(serial0.read()); //Call JY901 data cope function
   }
-  return -((float)JY901_1.stcAngle.Angle[1]/32768*180);
+  raw_stick_angle = -((float)JY901_1.stcAngle.Angle[1]/32768*180);
+  location = ((float)JY901_1.stcAngle.Angle[0] < 0 )? 1:0;
   
 }
 
-float read_boom(){
+void read_boom_base(){
  JY901_0.GetAngle();
- return ((float)JY901_0.stcAngle.Angle[1]/32768*180);
+ boom_angle = ((float)JY901_0.stcAngle.Angle[1]/32768*180);
+ raw_base_angle = ((float)JY901_0.stcAngle.Angle[2]/32768*180);
 }
 
-float read_base(){
-  JY901_0.GetAngle();
-  return ((float)JY901_0.stcAngle.Angle[2]/32768*180);
+float real_base_angle(float raw_base_angle){
+  float result = raw_base_angle - base_init;
+  if(base_init < -90 && raw_base_angle >= 0){
+    result = -360-base_init+raw_base_angle;// -180-base_init-(180-raw_base_angle)
+  }
+  else if (base_init > 90 && raw_base_angle <0){
+    result = 360-base_init + raw_base_angle;//180-base_init + raw_base_angle+180;
+  }
+  return result;
 }
 /*
   SerialEvent occurs whenever a new data comes in the
