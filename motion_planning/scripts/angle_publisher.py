262c53f0e44e384e8d1ec8af4fff158707b87b94
Liu Shiji <shijiliu@umich.edu>
2021-07-05 08:18:40 -0400

    feat: directly read in serial input angles

--- motion_planning/scripts/angle_publisher.py
+++ motion_planning/scripts/angle_publisher.py
@@ -5,20 +5,40 @@ import rospy
 import numpy as np
 import time
 from motion_planning.msg import excavator_angles
+import serial
+
+
+
+
 
 def talker():
-    pub_p = rospy.Publisher('target_angles', excavator_angles, queue_size=1)
-    rospy.init_node('talker', anonymous=True)
+    pub_p = rospy.Publisher('measured_angles', excavator_angles, queue_size=1)
+    rospy.init_node('measured_angles', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    array = np.array([0,0,0])
-    while not rospy.is_shutdown():
-        array += 1
-        left_top = excavator_angles()
-        left_top.angle0 = array[0]
-        left_top.angle1 = array[1]
-        left_top.angle2 = array[2]
 
-        print("%f,%f,%f"%(left_top.angle0,left_top.angle1,left_top.angle2))
+    #ser = serial.Serial(port='/dev/ttyUSB0',baudrate=9600)
+    left_top = excavator_angles()
+
+    s = serial.Serial(port='/dev/ttyACM0',baudrate=9600)
+    
+    while not rospy.is_shutdown():
+        s.flushInput()
+        raw_data = s.readline()
+        raw_data = s.readline()
+        if raw_data:
+            data = raw_data.split(',')
+            #print(data)
+
+            left_top.angle_stick = float(data[3])
+            left_top.angle_boom = float(data[2])
+            left_top.angle_base = float(data[4].split('\r')[0])         
+
+                
+                
+            
+            
+
+        print("%f,%f,%f"%(left_top.angle_stick,left_top.angle_boom,left_top.angle_base))
         pub_p.publish(left_top)
         rate.sleep()
  
