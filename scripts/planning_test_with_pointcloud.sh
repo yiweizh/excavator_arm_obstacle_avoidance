57c45a1e3b45910cd3f1c563e6f3b3333d9e0e97
Liu Shiji <shijiliu@umich.edu>
2021-07-30 17:46:31 +0800

    feat: visualization param & quick start scripts

--- scripts/planning_test_with_pointcloud.sh
+++ scripts/planning_test_with_pointcloud.sh
@@ -5,22 +5,18 @@
 #
 #xterm -e "roscore" &
 #sleep 5
-
-# open realsense camera
-xterm -e "roslaunch realsense2_camera rs_camera.launch" & 
-sleep 2
-
+ 
 # publish pointclouds
-xterm -e "rosrun motion_planning point_cloud_creater.py" &
+xterm -e "rosrun motion_planning point_cloud_publisher.py" &
 sleep 2
 
 # open rviz to visualize the published data, remember to open the configuration after rviz is open
-xterm -e "rosrun rviz rviz"
-#sleep 2
+xterm -e "rosrun rviz rviz" &
+sleep 2
 
 # start path planning
 xterm -e "rosrun motion_planning high_level_motion_planning.py" & 
 sleep 2
 
 # (optional) start rqt_gui, comment this line when low level control is started
-#xterm -e "rosrun rqt_gui rqt_gui" & 
+xterm -e "rosrun rqt_gui rqt_gui"
